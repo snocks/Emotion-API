f9718c48f3cb23dc473b61c7cd7726b5f0e827de
4daliaomry4 <alaakhattab92@gmail.com>
2017-02-14 11:41:34 +0200

    add array of images and randomImages function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,4 @@
 function request(config , cb) {
-	console.log("Three");
 	var xhr = new XMLHttpRequest();
 	xhr.open(config.method,config.url, true);
 	xhr.setRequestHeader("Content-Type","application/json");
@@ -7,7 +6,6 @@ function request(config , cb) {
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState == 4 && xhr.status == 200) {
 			var result = JSON.parse(xhr.responseText);
-			console.log("four");
 			cb(undefined, result);
 		}else if(xhr.readyState == 4 && xhr.status !== 200){
 			alert("Fail");
@@ -16,13 +14,11 @@ function request(config , cb) {
 	var data = '{"url":"' + config.urlImage + '"}';
 	xhr.send(data);
 }
-
-function analyzFace(faceUrl, cb) {
-	console.log("Two");
+function analyzFace(images, cb) {
 	request({
 		method: "POST",
 		url: "https://api.projectoxford.ai/emotion/v1.0/recognize",
-		urlImage: faceUrl,
+		urlImage: randomImages(images)
 		}, 
 		function( error,data) {
 			if(isHappy(data)){
@@ -33,7 +29,7 @@ function analyzFace(faceUrl, cb) {
 			}else{
 				cb(undefined, {
 					isHappy:false,
-					mess:'we ill make you so happy'
+					mess:'we will make you so happy'
 				});
 			}
 	});
@@ -41,4 +37,17 @@ function analyzFace(faceUrl, cb) {
 function isHappy(data){
 	return data[0].scores.happiness > data[0].scores.sadness;
 }
-var faceUrl = "http://www.drodd.com/images15/face13.jpg";
+function randomImages(list) {
+	return list[Math.floor(Math.random() * list.length)];
+ 
+}
+var images = [  'http://www.drodd.com/images15/face13.jpg',
+				'https://i.imgflip.com/z1s6k.jpg',
+				'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTYw3g5ng-GSkxdAMj--vagiT2WXTV2HjUV791q0Dbw6bpLCQuH',
+				'https://s-media-cache-ak0.pinimg.com/236x/8d/22/83/8d2283fba6942b8c073c14ee61e3dd28.jpg',
+				'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQtebs7rwGfDGLimv2wcGnOrBkjlYkTSTxMrIYvnKqFetIlgWYK',
+				'http://www.iwallfree.com/wp-content/uploads/2013/12/cute_baby_pics_face_girl_hd_wallpaper_4___cool_bab_10951_wallpaper-1024x768.jpg',
+				'https://www.brown-eyedgirlphotography.ca/blog/wp-content/uploads/2011/12/happy-face-baby-regina-photographer(pp_w880_h586).jpg',
+				'http://webneel.com/daily/sites/default/files/images/project/baby-photography%20(1).jpg',
+				'https://s-media-cache-ak0.pinimg.com/236x/00/ec/17/00ec171f5122823b091cf2064a0ea14a.jpg'
+];
