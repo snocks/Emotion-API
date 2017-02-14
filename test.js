f62a13999750aad44e119d05b14dabc13607c9d8
4daliaomry4 <alaakhattab92@gmail.com>
2017-02-14 11:43:05 +0200

    add tests for the analysFace function

--- test.js
+++ test.js
@@ -3,16 +3,34 @@ test("Function `request` should be exist", function(t) {
 	t.ok(window.request,'it exists');
 });
 
-test("request function should return Array",function(t){
+test("'request' function should return Array",function(t){
 	var done = t.async();
-	console.log("One");
-	analyzFace("http://www.drodd.com/images15/face13.jpg", function( error,data) {
-		console.log("error",error);
-		console.log("data",data);
-		t.equal(data.mess,"Fantastic, Keep going in your Happiness :)",'Get right messege');
+	request({
+		method: "POST",
+		url: "https://api.projectoxford.ai/emotion/v1.0/recognize",
+		urlImage: "http://www.drodd.com/images15/face13.jpg"
+		}, function(error, data) {
+			t.equal(Object.prototype.toString.call(data),'[object Array]','Get right messege');
+			done();
+	});
+  
+});
+	
+test("'analyzFace' function should return the text 'we will make you so happy' if image was sad",function(t){
+	var done = t.async();
+	analyzFace(images, function( error,data) {
+		t.equal(data.mess,'we will make you so happy','Get right messege');
 		done();
 	});
 
 });
 
+test("'analyzFace' function should return the text 'Fantastic, Keep going in your Happiness :)' if image was happy",function(t){
+	var done = t.async();
+	analyzFace(images, function( error,data) {
+		t.equal(data.mess,'Fantastic, Keep going in your Happiness :)','Get right messege');
+		done();
+	});
+
+});
 
