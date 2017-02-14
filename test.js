e4921ffc8d4ca257e3f6287c79953519e794108a
4daliaomry4 <alaakhattab92@gmail.com>
2017-02-14 14:56:11 +0200

    put images array in test.js file

--- test.js
+++ test.js
@@ -1,3 +1,13 @@
+var images = [  'http://www.drodd.com/images15/face13.jpg',
+				'https://i.imgflip.com/z1s6k.jpg',
+				'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTYw3g5ng-GSkxdAMj--vagiT2WXTV2HjUV791q0Dbw6bpLCQuH',
+				'https://s-media-cache-ak0.pinimg.com/236x/8d/22/83/8d2283fba6942b8c073c14ee61e3dd28.jpg',
+				'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQtebs7rwGfDGLimv2wcGnOrBkjlYkTSTxMrIYvnKqFetIlgWYK',
+				'http://www.iwallfree.com/wp-content/uploads/2013/12/cute_baby_pics_face_girl_hd_wallpaper_4___cool_bab_10951_wallpaper-1024x768.jpg',
+				'https://www.brown-eyedgirlphotography.ca/blog/wp-content/uploads/2011/12/happy-face-baby-regina-photographer(pp_w880_h586).jpg',
+				'http://webneel.com/daily/sites/default/files/images/project/baby-photography%20(1).jpg',
+				'https://s-media-cache-ak0.pinimg.com/236x/00/ec/17/00ec171f5122823b091cf2064a0ea14a.jpg'
+];
 var test = QUnit.test;
 test("Function `request` should be exist", function(t) {
 	t.ok(window.request,'it exists');
